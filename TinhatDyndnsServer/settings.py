9262d32b29336c88051cb2ba9429d9a0bff662f6
Christoph Emonds <info@christoph-emonds.de>
2013-11-14 20:20:57 +0100

    - added rest api app

--- TinhatDyndnsServer/settings.py
+++ TinhatDyndnsServer/settings.py
@@ -32,10 +32,10 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = (
     'django.contrib.admin',
     'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
+#    'django.contrib.contenttypes',
+#    'django.contrib.sessions',
+#    'django.contrib.messages',
+#    'django.contrib.staticfiles',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -43,8 +43,8 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+#    'django.contrib.messages.middleware.MessageMiddleware',
+#    'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
 ROOT_URLCONF = 'TinhatDyndnsServer.urls'
