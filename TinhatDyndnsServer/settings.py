5e09af63ca468e076ce7b8d06b1f38aba9d8770c
Christoph Emonds <info@christoph-emonds.de>
2013-11-14 21:15:42 +0100

    - defined model

--- TinhatDyndnsServer/settings.py
+++ TinhatDyndnsServer/settings.py
@@ -32,10 +32,11 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = (
     'django.contrib.admin',
     'django.contrib.auth',
-#    'django.contrib.contenttypes',
+    'django.contrib.contenttypes',
 #    'django.contrib.sessions',
 #    'django.contrib.messages',
 #    'django.contrib.staticfiles',
+    'restapi',
 )
 
 MIDDLEWARE_CLASSES = (
--- restapi/models.py
+++ restapi/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Hostname(models.Model):
+    hostname = models.CharField(max_length=32)
+    keyFingerprint = models.CharField(max_length=64)
+
+class AddressUpdate(models.Model):
+    hostname = models.ForeignKey(Hostname)
+    ipv4 = models.CharField(max_length=15)
+    ipv6 = models.CharField(max_length=45)
