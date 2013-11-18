4094223f5944b714ed27b3615560f7557dc7e67a
Christoph Emonds <info@christoph-emonds.de>
2013-11-18 23:00:49 +0100

    - removed rest_framework

--- TinhatDyndnsServer/settings.py
+++ TinhatDyndnsServer/settings.py
@@ -36,7 +36,6 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework',
     'restapi',
 )
 
@@ -52,19 +51,6 @@ MIDDLEWARE_CLASSES = (
 ROOT_URLCONF = 'TinhatDyndnsServer.urls'
 
 WSGI_APPLICATION = 'TinhatDyndnsServer.wsgi.application'
-
-REST_FRAMEWORK = {
-    # Use hyperlinked styles by default.
-    # Only used if the `serializer_class` attribute is not set on a view.
-    'DEFAULT_MODEL_SERIALIZER_CLASS':
-        'rest_framework.serializers.HyperlinkedModelSerializer',
-
-    # Use Django's standard `django.contrib.auth` permissions,
-    # or allow read-only access for unauthenticated users.
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-    ]
-}
 
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
--- restapi/views.py
+++ restapi/views.py
@@ -1,13 +1,32 @@
-from rest_framework.generics import GenericAPIView
-from rest_framework.mixins import CreateModelMixin, RetrieveModelMixin, UpdateModelMixin, DestroyModelMixin
-from rest_framework.response import Response
-from rest_framework import authentication, permissions
+from django.http import HttpResponseNotFound, HttpResponseNotAllowed, HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 from restapi.models import Hostname, AddressUpdate
 
-# Create your views here.
-class HostnameView(GenericAPIView, CreateModelMixin, RetrieveModelMixin, UpdateModelMixin, DestroyModelMixin):
-    """
-    View to list all users in the system.
-    """
-    model = Hostname
+@csrf_exempt
+def single_host(request, hostname):
+    if request.method == 'POST':
+        return create_new_host(request, hostname)
+    try:
+        host = Hostname.objects.get(hostname=hostname)
+    except Hostname.DoesNotExist:
+        return HttpResponseNotFound('<h1>Hostname not found</h1>')
+    if request.method == 'GET':
+        return get_host(request, host)
+    elif request.method == 'PUT':
+        return update_host(request, host)
+    elif request.method == 'DELETE':
+        return delete_host(request, host)
 
+    return HttpResponseNotAllowed('<h1>Method not allowed</h1>')
+
+def create_new_host(request, hostname):
+    return HttpResponse('<h1>Page was found</h1>')
+
+def get_host(request, host):
+    return HttpResponse('<h1>Page was found</h1>')
+
+def update_host(request, host):
+    return HttpResponse('<h1>Page was found</h1>')
+
+def delete_host(request, host):
+    return HttpResponse('<h1>Page was found</h1>')
