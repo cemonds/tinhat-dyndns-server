704fbfe3d4f54c8d4bbfa8ffe41f5df3ca0399e4
Christoph Emonds <info@christoph-emonds.de>
2013-11-27 15:36:01 +0100

    - add root directory of project to python path

--- TinhatDyndnsServer/wsgi.py
+++ TinhatDyndnsServer/wsgi.py
@@ -8,6 +8,12 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+package_path = os.path.dirname(__file__)
+root = os.path.dirname(package_path)
+sys.path.append(root)
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "TinhatDyndnsServer.wsgisettings")
 
 from django.core.wsgi import get_wsgi_application
